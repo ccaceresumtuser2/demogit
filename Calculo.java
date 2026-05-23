934e608787a6a1a7fc3b51fafa9d87fbf0159a15
ccaceresumtuser2 <91487385+ccaceresumtuser2@users.noreply.github.com>
2026-05-23 11:10:49 -0500

    Create Calculo.java

---
+++ Calculo.java
@@ -0,0 +1,21 @@
+public class Calculo {
+    private double salario;
+
+    public Calculo(double salario) {
+        this.salario = salario;
+    }
+    
+   
+    public double calculoSaludo(){
+        return salario*0.04;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+}
